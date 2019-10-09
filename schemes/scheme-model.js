d7dce041a396d8e2dbef6875dba47111deea4b91
Sara Alkhamri <s.alkhamri@gmail.com>
2019-10-09 14:25:26 -0700

    All helper methods created and successful in scheme-model.js

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -21,15 +21,40 @@ function findById(id) {
     .where({id})
     .first();
 }
-function findSteps() {
-    return null
+//Expects a scheme id.
+//Resolves to an array of all correctly ordered step for the given scheme: [ { id: 17, scheme_name: 'Find the Holy Grail', step_number: 1, instructions: 'quest'}, { id: 18, scheme_name: 'Find the Holy Grail', step_number: 2, instructions: '...and quest'}, etc. ].
+//This array should include the scheme_name not the scheme_id.
+function findSteps(id) {
+    return db('steps') 
+    .where({'steps.scheme_id': id})
+    .select('steps.id', 'schemes.scheme_name', 'steps.step_number', 'steps.instructions')
+    .join('schemes', 'schemes.id', 'steps.scheme_id')
 }
-function add() {
-    return null
+// Expects a scheme object.
+// Inserts scheme into the database.
+// Resolves to the newly inserted scheme, including id.
+function add(scheme) {
+    return db('schemes')
+    .insert(scheme, 'id')
+    .then(scheme => {
+        return scheme
+    })
 }
-function update() {
-    return null
+// Expects a changes object and an id.
+// Updates the scheme with the given id.
+// Resolves to the newly updated scheme object.
+function update(changes, id) {
+    return db('schemes')
+    .update(changes)
+    .where({id})
+    .then(updated => updated ? findById(id) : null)
 }
-function remove() {
-    return null
+// Removes the scheme object with the provided id.
+// Resolves to the removed scheme
+// Resolves to null on an invalid id.
+// (Hint: Only worry about removing the scheme. The database is configured to automatically remove all associated steps.)
+function remove(id) {
+    return db('schemes')
+    .where({id})
+    .del();
 }
